653f19fa937eb4f302654af59d9f6e0fc3442d49
mikeaco <michael.acolatse@gmail.com>
2023-04-18 10:24:05 -0400

    fix-sidebar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,20 @@
 import NavBar from "./NavBar";
 import { ProSidebarProvider } from 'react-pro-sidebar';
+import styled from 'styled-components';
 
-
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 function HomePage() {
   return (
-    <ProSidebarProvider>
-      <NavBar />
-    </ProSidebarProvider>
+    <Container>
+        <ProSidebarProvider>
+          <NavBar />
+        </ProSidebarProvider>
+      <div style={{fontSize:'36px'}}>Projects</div>
+    </Container>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,46 @@
 import React from "react";
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import styled from 'styled-components';
 
 const NavTitle = styled.h1`
-font-style: normal;
-font-weight: 400;
-font-size: 36px;
-line-height: 42px;
-
-color: #BCB1B1;
-margin-left: 5%;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 42px;
+    color: #BCB1B1;
+    margin-left: 5%;
+    margin-top: 0%
 `;
-
 
 function NavBar() {
     return (
-            <Sidebar backgroundColor="#332E2E;"
-                rootStyles={{
-                    position: 'absolute',
-                    height: '100%',
-                }} >
-                <NavTitle> Planify </NavTitle>
-                <Menu
-                    menuItemStyles={{
-                        button: ({ level, active, disabled }) => {
-                            // only apply styles on first level elements of the tree
-                            if (level === 0){
-                                return {
-                                  color: disabled ? "#f5d9ff" : "#ffffff",
-                                  backgroundColor: active
-                                    ? "#eecef9"
-                                    : undefined,
-                                  ":hover": { backgroundColor: "#8f9094" },
-                                };
-                            }
-
-                        },
-                    }}
-                >
-                    <MenuItem> Home </MenuItem>
-                    <MenuItem> My Account </MenuItem>
-                    <MenuItem> Log Out</MenuItem>
-                </Menu>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <Sidebar backgroundColor="#332E2E;">
+                <div style={{ flex: 1, height: '100vh' }}>
+                    <NavTitle> Planify </NavTitle>
+                    <Menu
+                        menuItemStyles={{
+                            button: ({ level, active, disabled }) => {
+                                // only apply styles on first level elements of the tree
+                                if (level === 0) {
+                                    return {
+                                        color: disabled ? "#f5d9ff" : "#ffffff",
+                                        backgroundColor: active
+                                            ? "#eecef9"
+                                            : undefined,
+                                        ":hover": { backgroundColor: "#8f9094" },
+                                    };
+                                }
+                            },
+                        }}
+                    >
+                        <MenuItem> Home </MenuItem>
+                        <MenuItem> My Account </MenuItem>
+                        <MenuItem> Log Out</MenuItem>
+                    </Menu>
+                </div>
             </Sidebar>
-
-
+        </div>
     );
 }
 
